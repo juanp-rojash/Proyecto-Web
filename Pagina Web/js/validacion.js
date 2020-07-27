bc1e2217ee52f19bc7d4b5b8db1506dea6a6c511
Juan Rojas <juanp.rojash@upb.edu.co>
2020-07-27 15:18:39 -0500

    Pagina

--- Pagina Web/js/validacion.js
+++ Pagina Web/js/validacion.js
@@ -1,33 +1,12 @@
-function validar(formulario){
 
-    var nombre = formulario.elements["nombre"].value;
-    var apellido = formulario.elements["apellido"].value;
-    var email = formulario.elements["correo"].value;
-    var mensaje = "";
-    var valido = true;
-    if(nombre == null || nombre.length == 0 || /^\s+$/.test(nombre) || /^[A-Z]+$/i.test(nombre) == false){
-        mensaje = mensaje + "-Ingrese de manera correcta su nombre\n"
-        formulario.elements["nombre"].setAttribute("style","color:red;");
-        valido = false;
-    }
+let pst, frm
 
-    if(apellido == null || apellido.length == 0 || /^\s+$/.test(apellido) || /^[A-Z]+$/i.test(apellido) == false){
-        mensaje = mensaje + "-Ingrese de manera correcta su apellido\n"
-        valido = false;
-    }
+window.onload = function () {
+    frm = document.getElementById("formulario");
+    pst = new Pristine(frm);
 
-    if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w/.test(email))){
-        mensaje = mensaje + "-Ingrese una direccion de correo valida\n  Ejemplo: nombre.apellido@gmail.com";
-        valido = false;
-    }
-    
-    if(valido == true){
-        alert("Se enviaron los datos exitosamente");
-        return true;
-    }
-    else{
-        alert(mensaje);
-        return false;
-    }
-
+    frm.addEventListener('submit', function(e){
+        e.preventDefault();
+        var valid = pst.validate();
+    })
 }
