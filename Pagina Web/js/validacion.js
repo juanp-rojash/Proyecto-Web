cecafd5e564549c8149ac21eed6e92572e266323
Juan Rojas <juanp.rojash@upb.edu.co>
2020-07-29 23:14:25 -0500

    Actualizacion

--- Pagina Web/js/validacion.js
+++ Pagina Web/js/validacion.js
@@ -4,9 +4,12 @@ let pst, frm
 window.onload = function () {
     frm = document.getElementById("formulario");
     pst = new Pristine(frm);
+    cargarDepartamentos();
 
     frm.addEventListener('submit', function(e){
         e.preventDefault();
         var valid = pst.validate();
-    })
-}
+        frm.classList.add("was-validated");
+        cargarDepartamentos();
+    });
+};
